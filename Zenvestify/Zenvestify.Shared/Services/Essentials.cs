10891d41941ae0d4222df179288cc34064e30c06
OudomMunint <oudommunint@gmail.com>
2025-09-05 17:50:29 +1000

    #16 - EssentialsHelper

---
+++ Zenvestify/Zenvestify.Shared/Services/Essentials.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Maui;
+
+namespace Zenvestify.Shared.Services
+{
+    public class Essentials
+    {
+        /// <summary>
+        /// Gets the current application version and build number.
+        /// </summary>
+        public static string GetAppVersion()
+        {
+            var version = VersionTracking.Default.CurrentVersion;
+            var build = VersionTracking.Default.CurrentBuild;
+            var platform = DeviceInfo.Platform;
+            return $"Zenvesity {platform} v {version} (Build {build})";
+        }
+
+        /// <summary>
+        /// Get metadata from VersionTracker
+        /// </summary>
+        public static string GetAppMetaData()
+        {
+            var tracker = VersionTracking.Default;
+            var metaData = new List<string>
+            {
+                tracker.IsFirstLaunchEver.ToString(),
+                tracker.IsFirstLaunchForCurrentVersion.ToString(),
+                tracker.IsFirstLaunchForCurrentBuild.ToString(),
+                tracker.CurrentVersion.ToString(),
+                tracker.CurrentBuild.ToString(),
+                String.Join(',', tracker.VersionHistory),
+                String.Join(',', tracker.BuildHistory),
+                tracker?.ToString() ?? "none",
+                tracker?.ToString() ?? "none",
+            };
+
+            return String.Join('\n', metaData);
+        }
+    }
+}
